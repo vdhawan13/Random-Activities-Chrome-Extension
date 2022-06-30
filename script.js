eda6befd2b31c53ba8f9155a3926d5c52f38b4c0
Vaibhav <88321272+vdhawan13@users.noreply.github.com>
2022-06-30 21:56:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 fetch('https://www.boredapi.com/api/activity')
     .then(a => a.json())
     .then(NewsData => {
-        const a=Math.floor(Math.random() * 100);
         const NewsText = "Activity:- "+NewsData.activity+"<br>"+"\nType of activity:- "+ NewsData.type+"<br>"+"\nParticipants:- "+ NewsData.participants;
         const NewsElement = document.getElementById('NewsElement');
 
